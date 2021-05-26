0579ebc4eda8be3f7f98a08bce459db57435f9da
Jake Robertson <jake@faltro.com>
2021-05-26 13:21:17 -0400

    add script to create extension list for readme

---
+++ scripts/create-markdown-table.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+OUTPATH="extension_table.md"
+
+echo "| ID | Name | URL | Notes |" >$OUTPATH
+echo "| - | - | - | - |" >>$OUTPATH
+
+for EXTENSION_DIR in ../extensions/*/; do
+  cd $EXTENSION_DIR
+
+  EXTENSION_NAME=$(jq -r '.name' metadata.json)
+  EXTENSION_ID=$(jq -r '.id' metadata.json)
+  EXTENSION_URL=$(jq -r '.url' metadata.json)
+
+  cd - >/dev/null
+  echo "| $EXTENSION_ID | $EXTENSION_NAME | $EXTENSION_URL | |" >>$OUTPATH
+done
