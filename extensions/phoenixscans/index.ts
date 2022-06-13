188c9ebf08900cbc2a14ff71cefdb5ca4043c1a9
Jake Robertson <jake@faltro.com>
2022-06-13 16:05:26 -0700

    phoenixscans: add source

---
+++ extensions/phoenixscans/index.ts
@@ -0,0 +1,86 @@
+import {
+  GetSeriesFunc,
+  GetChaptersFunc,
+  GetPageRequesterDataFunc,
+  GetPageUrlsFunc,
+  GetSearchFunc,
+  GetPageDataFunc,
+  ExtensionMetadata,
+  GetDirectoryFunc,
+  ExtensionClientAbstract,
+  FetchFunc,
+  WebviewFunc,
+  Series,
+  PageRequesterData,
+  SeriesSourceType,
+  SetSettingsFunc,
+  GetSettingsFunc,
+  GetSettingTypesFunc,
+} from "houdoku-extension-lib";
+import DOMParser from "dom-parser";
+import metadata from "./metadata.json";
+import { PizzaReaderClient } from "../../generic/pizzareader";
+import { parseMetadata } from "../../util/configuring";
+
+export const METADATA: ExtensionMetadata = parseMetadata(metadata);
+
+export class ExtensionClient extends ExtensionClientAbstract {
+  pizzaReaderClient: PizzaReaderClient;
+
+  constructor(
+    fetchFn: FetchFunc,
+    webviewFn: WebviewFunc,
+    domParser: DOMParser
+  ) {
+    super(fetchFn, webviewFn, domParser);
+    this.pizzaReaderClient = new PizzaReaderClient(
+      METADATA.id,
+      "https://www.phoenixscans.com",
+      fetchFn
+    );
+  }
+
+  getMetadata: () => ExtensionMetadata = () => {
+    return METADATA;
+  };
+
+  getSeries: GetSeriesFunc = (sourceType: SeriesSourceType, id: string) =>
+    this.pizzaReaderClient.getSeries(sourceType, id);
+
+  getChapters: GetChaptersFunc = (sourceType: SeriesSourceType, id: string) =>
+    this.pizzaReaderClient.getChapters(sourceType, id);
+
+  getPageRequesterData: GetPageRequesterDataFunc = (
+    sourceType: SeriesSourceType,
+    seriesSourceId: string,
+    chapterSourceId: string
+  ) =>
+    this.pizzaReaderClient.getPageRequesterData(
+      sourceType,
+      seriesSourceId,
+      chapterSourceId
+    );
+
+  getPageUrls: GetPageUrlsFunc = (pageRequesterData: PageRequesterData) =>
+    this.pizzaReaderClient.getPageUrls(pageRequesterData);
+
+  getPageData: GetPageDataFunc = (series: Series, url: string) =>
+    this.pizzaReaderClient.getPageData(series, url);
+
+  getSearch: GetSearchFunc = (
+    text: string,
+    params: { [key: string]: string },
+    page: number
+  ) => this.pizzaReaderClient.getSearch(text, params, page);
+
+  getDirectory: GetDirectoryFunc = (page: number) =>
+    this.pizzaReaderClient.getDirectory(page);
+
+  getSettingTypes: GetSettingTypesFunc = () =>
+    this.pizzaReaderClient.getSettingTypes();
+
+  getSettings: GetSettingsFunc = () => this.pizzaReaderClient.getSettings();
+
+  setSettings: SetSettingsFunc = (newSettings: { [key: string]: any }) =>
+    this.pizzaReaderClient.setSettings(newSettings);
+}
